7af4f1e3792f97a070f566f9cfd4d97503842a4e
Kai <33795285+kaiicheng@users.noreply.github.com>
2023-10-02 18:30:44 -0400

    Delete pages directory

--- pages/api/demo.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-// fetch data from 'bucket' and return it as JSON
-import dataDemo from "../../data/dataDemo.json"
-
-// feat: pull data and return to API as route
-export default function handler(req, res) {
-  res.status(200).json(dataDemo)
-}
