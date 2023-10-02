4e1163437f71c2186873d1032ba9dd1becabdcd8
Kai <33795285+kaiicheng@users.noreply.github.com>
2023-10-02 18:31:26 -0400

    Delete KafkaTest directory

--- KafkaTest/kc.py
+++
@@ -1,4 +0,0 @@
-from kafka import KafkaConsumer
-consumer = KafkaConsumer('test')
-for message in consumer:
-    print (message)
--- KafkaTest/kp.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-future=producer.send('test', b'Hello, World!')
-result = future.get(timeout=60)
-producer.send('test', key=b'message-two', value=b'This is Kafka-Python')
-producer.flush()
-
